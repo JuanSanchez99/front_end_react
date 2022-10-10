f42fd5d3fbcc970f1a5c686ad290f3d2a83a4a46
Juan Sanchez <juan.camilosanchez@hotmail.com>
2022-10-10 12:44:50 -0500

    change api url

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const domain = "http://localhost:8000/";
+const domain = "https://django-backend-yqt9.onrender.com/";
 
 export const api = axios.create({
   baseURL: domain + "api/",
